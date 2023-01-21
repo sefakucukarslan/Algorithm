552f6ba4e111a498bae74bb2dc64b8fe992bc2a9
Sefa <sefak.arslan96@gmail.com>
2023-01-21 22:17:31 +0300

    Add project files.

---
+++ MaxMinNumber/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MaxMinNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = { 5, 8, 1, 3, 9, 2, 7, 6, 4};
+
+            int max = numbers[0];
+            int min = numbers[0];
+
+            for (int i = 1; i < numbers.Length; i++)
+            {
+                if (numbers[i] > max)
+                {
+                    max = numbers[i];
+                }
+                if (numbers[i] < min)
+                {
+                    min = numbers[i];
+                }
+            }
+            Console.WriteLine("Maximum: " + max);
+            Console.WriteLine("Minimum: " + min);
+        }
+    }
+}
---
+++ OddEvenNumbers/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OddEvenNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+
+            var evenNumbers = numbers.Where(x => x % 2 == 0);
+
+            var oddNumbers = numbers.Where(x => x % 2 != 0);
+
+            Console.WriteLine("Even numbers: ");
+            foreach (var number in evenNumbers)
+            {
+                Console.Write(number + " ");
+            }
+
+            Console.WriteLine("\nOdd numbers: ");
+            foreach (var number in oddNumbers)
+            {
+                Console.Write(number + " ");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
