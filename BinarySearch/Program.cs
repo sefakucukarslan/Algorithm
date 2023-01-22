2276b7fc920e92a610de7c6712e2493cd29609f4
Sefa <sefak.arslan96@gmail.com>
2023-01-22 20:45:51 +0300

    Binary Search

--- BinarySearch/Program.cs
+++ BinarySearch/Program.cs
@@ -22,21 +22,6 @@ namespace BinarySearch
                     left = mid - 1;
             }
             return -1;
-
-
-
-            //int left = 0, right = arr.Length - 1;
-            //while (left <= right)
-            //{
-            //    int mid = (left + right) / 2;
-            //    if (arr[mid] == x)
-            //        return mid;
-            //    else if (arr[mid] < x)
-            //        left = mid + 1;
-            //    else
-            //        right = mid - 1;
-            //}
-            //return -1;
         }
 
         static void Main(string[] args)
