513db7763026529c22588aef474648ce05dd7a1d
Sefa <sefak.arslan96@gmail.com>
2023-01-23 08:39:17 +0300

    Added Insertion Sort and Fibonacci

---
+++ Fibonacci/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fibonacci
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n1 = 0, n2 = 1, n3, i, number;
+
+            Console.Write("Enter the number of elements: ");
+
+            number = int.Parse(Console.ReadLine());
+
+            Console.Write(n1 + " " + n2 + " ");
+
+            for (i = 1; i < number; ++i)
+            {
+                n3 = n1 + n2;
+                Console.Write(n3 + " ");
+                n1 = n2;
+                n2 = n3;
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ InsertionSort/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InsertionSort
+{
+    internal class Program
+    {
+        static void Sort(int[] arr)
+        {
+            int n = arr.Length;
+            for (int i = 1; i < n; ++i)
+            {
+                int key = arr[i];
+                int j = i - 1;
+
+                while (j >= 0 && arr[j] > key)
+                {
+                    arr[j + 1] = arr[j];
+                    j--;
+                }
+                arr[j + 1] = key;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            int[] arr = { 12, 11, 13, 5, 6 };
+
+            Console.WriteLine("Original array : ");
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.Write(arr[i] + " ");
+            }
+
+            Sort(arr);
+
+            Console.WriteLine("\nSorted array : ");
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.Write(arr[i] + " ");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
