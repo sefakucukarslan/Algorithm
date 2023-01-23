82e15035a68827ac04264e053b87a137d81a1e82
Sefa <sefak.arslan96@gmail.com>
2023-01-23 15:29:41 +0300

    Added Bubble Sort example

---
+++ BubbleSort/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BubbleSort
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = { 3, 7, 4, 2, 8, 6, 1, 5, 9, 0 };
+
+            Console.WriteLine("Original array : ");
+            PrintArray(array);
+
+            bubbleSort(array);
+
+            Console.WriteLine("Sorted array : ");
+            PrintArray(array);
+
+            Console.ReadLine();
+        }
+
+        static void PrintArray(int[] array)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write(array[i] + " ");
+            }
+            Console.WriteLine();
+        }
+        static void bubbleSort(int[] array)
+        {
+            int temp;
+            for (int i = 0; i < array.Length - 1; i++)
+            {
+                for (int j = 0; j < array.Length - i - 1; j++)
+                {
+                    if (array[j] > array[j + 1])
+                    {
+                        temp = array[j];
+                        array[j] = array[j + 1];
+                        array[j + 1] = temp;
+                    }
+                }
+            }
+        }
+    }
+}
