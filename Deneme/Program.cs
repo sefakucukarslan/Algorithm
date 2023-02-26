e1dec14e5c8956399811af1e1dc8f8a98a880522
Sefa <sefak.arslan96@gmail.com>
2023-02-26 22:16:58 +0300

    Added Reversing Numbers

---
+++ Deneme/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Deneme
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.Write("Sayı gir : ");
+            //int n = Convert.ToInt32(Console.ReadLine());
+
+            //for (int i = 0; i < n; i++)
+            //{
+            //    for (int j = n - 1; j > i; j--)
+            //    {
+            //        Console.Write(" ");
+            //    }
+            //    for (int t = 0; t <= i; t++)
+            //    {
+            //        Console.Write("*");
+            //    }
+            //    Console.WriteLine();
+            //}
+            //Console.ReadLine();
+        }
+    }
+}
---
+++ ReversingNumbers/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReversingNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num, rev = 0;
+
+            Console.Write("Enter any number: ");
+            num = Convert.ToInt32(Console.ReadLine());
+
+            while (num > 0)
+            {
+                rev = rev * 10;
+                rev = rev + num % 10;
+                num = num / 10;
+            }
+
+            Console.Write("Reversed number is = " + rev);
+
+            Console.ReadLine();
+        }
+    }
+}
